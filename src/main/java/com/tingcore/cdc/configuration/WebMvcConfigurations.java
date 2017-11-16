f62ab3559e030c5fb2cb7cccb54cdd92e1820ff9
Palmi Valgeirsson <palmi.valgeirsson@cygni.se>
2017-11-16 11:54:43 +0100

    CDC-808: Add CORS mapping

---
+++ src/main/java/com/tingcore/cdc/configuration/WebMvcConfigurations.java
@@ -0,0 +1,40 @@
+package com.tingcore.cdc.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.UrlPathHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class WebMvcConfigurations extends WebMvcConfigurerAdapter {
+
+    private List<String> allowedOrigins = new ArrayList<>();
+
+
+    /**
+     * Override needed to enable matrix parameters in endpoints.
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins.toArray(new String[allowedOrigins.size()]));
+    }
+
+
+    @Value("#{'${app.allowed-origins}'.split(',')}")
+    public void set(final List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}

26cf2afd0ad30bf7915411c99dba0a4f3b09bfc8
Mats Fredriksson <mats.fredriksson@cygni.se>
2017-12-11 16:32:40 +0100

    Adding property file for stage, plus config for charging.

--- src/main/java/com/tingcore/cdc/charging/config/AssetServiceClientConfig.java
+++ src/main/java/com/tingcore/cdc/charging/config/AssetServiceClientConfig.java
@@ -5,17 +5,23 @@ import com.tingcore.charging.assets.api.ChargePointModelsApi;
 import com.tingcore.charging.assets.api.ChargeSitesApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 
 @Configuration
 public class AssetServiceClientConfig {
 
+    private final String assetServiceBaseUrl;
+
+    public AssetServiceClientConfig(Environment env) {
+        this.assetServiceBaseUrl = env.getRequiredProperty("app.asset-service.base-url");
+    }
+
     @Bean
     public ChargeSitesApi createChargeSiteControllerApi() {
-
         ApiClient client = new ApiClient();
-        //client.getAdapterBuilder().baseUrl("http://localhost:8888");
         client.getAdapterBuilder().baseUrl("https://asset-service-test.tingcore-infra.com");
+        client.getAdapterBuilder().baseUrl(this.assetServiceBaseUrl);
         return client.createService(ChargeSitesApi.class);
     }
 
--- src/main/java/com/tingcore/cdc/charging/config/OperationServiceClientConfig.java
+++ src/main/java/com/tingcore/cdc/charging/config/OperationServiceClientConfig.java
@@ -4,17 +4,22 @@ import com.tingcore.charging.operations.ApiClient;
 import com.tingcore.charging.operations.api.OperationsApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class OperationServiceClientConfig {
 
 
+    private final String chargingOperationsServiceBaseUrl;
+
+    public OperationServiceClientConfig(Environment env) {
+        this.chargingOperationsServiceBaseUrl = env.getRequiredProperty("app.charging-operations-service.base-url");
+    }
+
     @Bean
     public OperationsApi createOperationsApi() {
         ApiClient client = new ApiClient();
-
-        //client.setBasePath("http://localhost:8080");
-        client.getAdapterBuilder().baseUrl("http://charging-operations.tingcore-test.com");
+        client.getAdapterBuilder().baseUrl(this.chargingOperationsServiceBaseUrl);
         return client.createService(OperationsApi.class);
     }
 

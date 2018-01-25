11abb43b5a80d579371082e036ba239ae05a38f5
Robin <robin.merell@nuway.se>
2018-01-25 11:26:09 +0100

    PR comment fix

--- src/main/java/com/tingcore/cdc/charging/repository/AssetPaymentsRepository.java
+++ src/main/java/com/tingcore/cdc/charging/repository/AssetPaymentsRepository.java
@@ -1,30 +1,18 @@
 package com.tingcore.cdc.charging.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableMap;
 import com.tingcore.cdc.charging.model.*;
-import com.tingcore.cdc.exception.NoSessionFoundException;
 import com.tingcore.charging.assets.api.ApiForPaymentsApi;
 import com.tingcore.charging.assets.model.ChargePointInfo;
 import com.tingcore.commons.api.repository.AbstractApiRepository;
 import com.tingcore.commons.external.ExternalApiException;
-import com.tingcore.payments.emp.api.ChargesApi;
-import com.tingcore.payments.emp.model.*;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestClientException;
 
-import java.time.Instant;
-import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 import static com.tingcore.cdc.controller.ApiUtils.getResponseOrThrowError;
 import static java.lang.String.format;
-import static java.util.Optional.ofNullable;
 import static org.apache.commons.lang3.Validate.notNull;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Repository
 public class AssetPaymentsRepository extends AbstractApiRepository {
@@ -38,10 +26,10 @@ public class AssetPaymentsRepository extends AbstractApiRepository {
     }
 
     public ChargePointInfo fetchChargePointInfo(final ConnectorId id) {
-        return getResponseOrAssetpaymentError(apiForPaymentsApi.findChargePointByConnectorIdUsingGET(id.value));
+        return getResponseOrAssetPaymentError(apiForPaymentsApi.findChargePointByConnectorIdUsingGET(id.value));
     }
 
-    private <T, E extends ExternalApiException> T getResponseOrAssetpaymentError(CompletableFuture<T> request) throws E {
+    private <T, E extends ExternalApiException> T getResponseOrAssetPaymentError(CompletableFuture<T> request) throws E {
         return getResponseOrThrowError(execute(request), AssetPaymentsApiException::new);
     }
 

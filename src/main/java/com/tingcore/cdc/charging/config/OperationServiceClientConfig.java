2eba345cb3c43975cb9bcdfdc25eb6257d33b49d
Johan Horvius <drhorv@gmail.com>
2018-01-26 14:53:27 +0100

    Updated client versions

--- src/main/java/com/tingcore/cdc/charging/config/OperationServiceClientConfig.java
+++ src/main/java/com/tingcore/cdc/charging/config/OperationServiceClientConfig.java
@@ -2,12 +2,16 @@ package com.tingcore.cdc.charging.config;
 
 import com.tingcore.charging.operations.ApiClient;
 import com.tingcore.charging.operations.api.OperationsApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 @Configuration
 public class OperationServiceClientConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OperationServiceClientConfig.class);
 
 
     private final String chargingOperationsServiceBaseUrl;
@@ -20,6 +24,9 @@ public class OperationServiceClientConfig {
     public OperationsApi createOperationsApi() {
         ApiClient client = new ApiClient();
         client.getAdapterBuilder().baseUrl(this.chargingOperationsServiceBaseUrl);
+        LOG.info("URL for Asset for OperationService: {}", this.chargingOperationsServiceBaseUrl);
+
+
         return client.createService(OperationsApi.class);
     }
 

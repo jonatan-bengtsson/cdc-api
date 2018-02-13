8933cb2b133c4642dd3ad7d336cb28aa752f520a
abhinav.goel <abhinav.goel@tingcore.com>
2018-02-13 15:28:30 +0100

    New api for connector operational status

--- src/main/java/com/tingcore/cdc/charging/mapper/OperationsApiMapper.java
+++ src/main/java/com/tingcore/cdc/charging/mapper/OperationsApiMapper.java
@@ -1,37 +1,26 @@
 package com.tingcore.cdc.charging.mapper;
 
-import com.tingcore.charging.assets.model.CompleteChargePoint;
 import com.tingcore.charging.assets.model.CompleteChargePointSite;
-import com.tingcore.charging.assets.model.Connector;
-import com.tingcore.charging.operations.model.ChargePointStatusRequest;
-import com.tingcore.charging.operations.model.StatusBatchRequest;
+import com.tingcore.charging.operations.model.IdList;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class OperationsApiMapper {
 
-    public static StatusBatchRequest toBatchStatusRequest(CompleteChargePointSite ccps) {
-        return new StatusBatchRequest()
-                .statusRequests(ccps.getChargePoints().stream()
-                        .map(OperationsApiMapper::toChargePointStatusRequest)
-                        .collect(Collectors.toList())
-                );
+    public static IdList toBatchStatusRequest(CompleteChargePointSite ccps) {
+        return new IdList().ids(ccps.getChargePoints().stream().map(cp -> cp.getChargePointEntity().getMetadata().getId())
+                               .collect(
+                Collectors.toList()));
     }
 
-    public static StatusBatchRequest toBatchStatusRequest(Stream<CompleteChargePointSite> ccpss) {
-        return new StatusBatchRequest()
-                .statusRequests(
+    public static IdList toBatchStatusRequest(Stream<CompleteChargePointSite> ccpss) {
+        return new IdList().ids(
                         ccpss.map(CompleteChargePointSite::getChargePoints)
-                                .flatMap(cps -> cps.stream().map(OperationsApiMapper::toChargePointStatusRequest))
+                                .flatMap(cps -> cps.stream().map(cp -> cp.getChargePointEntity().getMetadata().getId()))
                         .collect(Collectors.toList())
                 );
-    }
 
-    public static ChargePointStatusRequest toChargePointStatusRequest(CompleteChargePoint cp) {
-        return new ChargePointStatusRequest()
-                .chargePointId(cp.getChargePointEntity().getMetadata().getId())
-                .connectorIds(cp.getConnectorEntities().stream()
-                        .map(ce -> ce.getMetadata().getId()).collect(Collectors.toList()));
     }
 
 }

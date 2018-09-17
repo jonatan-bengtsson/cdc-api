4dac3d138f3e99e19966d43170e306cbf43b9cff
Daniel Vilhelmsson <daniel.vilhelmsson@comhem.com>
2018-09-17 16:27:23 +0200

    create elwin contract when key added

--- src/main/java/com/tingcore/cdc/crm/controller/v1/CustomerKeyController.java
+++ src/main/java/com/tingcore/cdc/crm/controller/v1/CustomerKeyController.java
@@ -7,6 +7,7 @@ import com.tingcore.cdc.crm.model.CustomerKey;
 import com.tingcore.cdc.crm.model.UserPaymentOption;
 import com.tingcore.cdc.crm.service.v1.CustomerKeyService;
 import com.tingcore.cdc.exception.EntityNotFoundException;
+import com.tingcore.cdc.payments.service.PaymentAccountService;
 import com.tingcore.commons.hash.HashIdService;
 import com.tingcore.commons.rest.ErrorResponse;
 import com.tingcore.commons.rest.PageResponse;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Optional;
 
 /**
  * @author palmithor
@@ -31,14 +33,17 @@ import javax.annotation.Resource;
 public class CustomerKeyController {
 
     private final CustomerKeyService customerKeyService;
+    private final PaymentAccountService paymentAccountService;
     private final HashIdService hashIdService;
 
     @Resource(name = WebMvcConfiguration.AUTHORIZED_USER)
     private AuthorizedUser authorizedUser;
 
     public CustomerKeyController(final CustomerKeyService customerKeyService,
+                                 final PaymentAccountService paymentAccountService,
                                  final HashIdService hashIdService) {
         this.customerKeyService = customerKeyService;
+        this.paymentAccountService = paymentAccountService;
         this.hashIdService = hashIdService;
     }
 
@@ -83,7 +88,20 @@ public class CustomerKeyController {
         final Long customerKeyId = hashIdService.decode(encodedCustomerKeyId).orElseThrow(() -> new EntityNotFoundException(CustomerKey.class.getSimpleName(), encodedCustomerKeyId));
         final Long userPaymentOptionId = hashIdService.decode(encodedUserPaymentOptionId).orElseThrow(() -> new EntityNotFoundException(UserPaymentOption.class.getSimpleName(), encodedCustomerKeyId));
 
-        return customerKeyService.addUserPaymentOption(authorizedUser.getId(), customerKeyId, userPaymentOptionId);
+        CustomerKey customerKey = customerKeyService.addUserPaymentOption(authorizedUser.getId(), customerKeyId, userPaymentOptionId);
+
+        Optional<UserPaymentOption> connectedPaymentOption = customerKey.getUserPaymentOptions().stream()
+                .filter(userPaymentOption -> userPaymentOption.getId().equals(userPaymentOptionId))
+                .findFirst();
+
+        if (connectedPaymentOption.isPresent() && connectedPaymentOption.get().getPaymentOptionReference().startsWith("ELWIN-")) {
+            String keyIdentifier = customerKey.getKeyIdentifier();
+            String elwinId = connectedPaymentOption.get().getPaymentOptionReference().split("-")[1];
+            paymentAccountService.createElwinContract(elwinId, keyIdentifier);
+        }
+
+        return customerKey;
+
     }
 
     @RequestMapping(method = RequestMethod.DELETE, path = "/{customerKeyId}/user-payment-options/{userPaymentOptionId}")

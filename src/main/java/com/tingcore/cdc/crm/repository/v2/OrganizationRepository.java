3bf2fa718cb606bef335de27251c3646e08f83ab
Palmi Valgeirsson <palmi.valgeirsson@cygni.se>
2018-09-27 09:01:22 +0200

    Fix build due to removed deprecated class

--- src/main/java/com/tingcore/cdc/crm/repository/v2/OrganizationRepository.java
+++ src/main/java/com/tingcore/cdc/crm/repository/v2/OrganizationRepository.java
@@ -3,8 +3,7 @@ package com.tingcore.cdc.crm.repository.v2;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tingcore.cdc.crm.repository.AbstractUserServiceRepository;
 import com.tingcore.commons.rest.repository.ApiResponse;
-import com.tingcore.users.api.v2.InternalApi;
-import com.tingcore.users.api.v2.OrganizationsApi;
+import com.tingcore.users.api.v2.InternalOrganizationApi;
 import com.tingcore.users.model.v2.response.Organization;
 import org.springframework.stereotype.Repository;
 
@@ -13,21 +12,19 @@ import java.util.List;
 @Repository
 public class OrganizationRepository extends AbstractUserServiceRepository {
 
-    private final InternalApi internalApi;
-    private final OrganizationsApi organizationsApi;
+    private final InternalOrganizationApi internalOrganizationApi;
 
-    OrganizationRepository(ObjectMapper objectMapper, InternalApi internalApi, OrganizationsApi organizationsApi) {
+    OrganizationRepository(ObjectMapper objectMapper, final InternalOrganizationApi internalOrganizationApi) {
         super(objectMapper);
-        this.internalApi = internalApi;
-        this.organizationsApi = organizationsApi;
+        this.internalOrganizationApi = internalOrganizationApi;
     }
 
     public ApiResponse<Organization> getOrganizationByUserPrefixInternal(final String userPrefix) {
-        return execute(internalApi.getByUserPrefix(userPrefix));
+        return execute(internalOrganizationApi.getByUserPrefix(userPrefix));
     }
 
     public ApiResponse<List<Long>> getAssociatedCpoIdsFromEmpId(final long empOrganizationId) {
-        return execute(organizationsApi.internalGetAssociatedCpoIds(empOrganizationId));
+        return execute(internalOrganizationApi.internalGetAssociatedCpoIds(empOrganizationId));
     }
 
 

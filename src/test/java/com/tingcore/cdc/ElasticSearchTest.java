6c05267298f0e9d26cd3e0e573ec9603aa48ec42
Daniel Forslund <df.forslund@gmail.com>
2019-02-06 14:25:35 +0100

    CND-1169: ES test for SessionHistoryRepository

---
+++ src/test/java/com/tingcore/cdc/ElasticSearchTest.java
@@ -0,0 +1,52 @@
+package com.tingcore.cdc;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.AfterClass;
+import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
+import pl.allegro.tech.embeddedelasticsearch.PopularProperties;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public abstract class ElasticSearchTest {
+    private static EmbeddedElastic embeddedElastic;
+    protected static RestHighLevelClient client;
+
+    protected static void setup(final String index, final String type, final Map<Object, String> testData) {
+        final EmbeddedElastic.Builder builder = EmbeddedElastic.builder()
+                .withElasticVersion("6.2.4")
+                .withSetting(PopularProperties.CLUSTER_NAME, "cnd");
+
+        builder.withIndex(index);
+
+        try {
+            embeddedElastic = builder.withStartTimeout(30, TimeUnit.SECONDS)
+                    .build()
+                    .start();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", embeddedElastic.getHttpPort(), "http")));
+
+        final BulkRequest bulkRequest = new BulkRequest();
+        testData.forEach((id, doc) -> bulkRequest.add(new IndexRequest(index, type, id.toString()).source(doc, XContentType.JSON)));
+        try {
+            client.bulk(bulkRequest);
+            Thread.sleep(3000);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        embeddedElastic.stop();
+    }
+}
